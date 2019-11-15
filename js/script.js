4d5648bb86db1e248cd5e6cdf46cd65ed77b409a
Zachary Tinubu-Karch <zacharytinubu-karch@zacharys-mbp.wireless.newschool.edu>
2019-11-15 16:21:26 -0500

    Input 2 assignment first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const userInput = window.prompt('Password?');
+const userOutputEl = document.getElementById('output');
+
+if (userInput!=null){
+	document.getElementById("welcome")=
+	"Hello" + userInput + "! Couldn't figure out how to make the password work!";
+}
+
+console.log(userInput);
+
+
+
+console.log('userOutputEl');
+
+
